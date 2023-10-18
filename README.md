3b3e6cf6b82f29b0bb19310cb4478b4c5143247d
Dean McNiven <deanmcniven@users.noreply.github.com>
2023-10-19 08:57:30 +1100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# mac-soe
+
+Ansible based SOE for configuring MacOS for stable usage.
+
+Note: This repo is quite opinionated on what I think is a correct configuration.
---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+BOLD='\033[1m'
+UNBOLD='\033[0m'
+
+if ! command -v brew &> /dev/null; then
+    echo -e "⛔️ ${BOLD}brew not found. Installing ...${UNBOLD}"
+    /bin/bash -c $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+    exit 1
+fi
+
+if ! command -v ansible &> /dev/null; then
+    echo -e "📦  ${BOLD}Installing Ansible${UNBOLD}"
+    brew install ansible
+fi
+
